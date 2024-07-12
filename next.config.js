75b0c6f31b0ecfe7df1772afee19d5e1daf0aa89
Joseph Thompson <joe.thompson838@gmail.com>
2024-07-12 11:22:19 -0400

    update next config for webpack alias

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com', 'firebasestorage.googleapis.com'],
   },
+  webpack: (config) => {
+    config.resolve.alias['@components'] = path.join(__dirname, 'components');
+    return config;
+  },
 };
 
 module.exports = nextConfig;
