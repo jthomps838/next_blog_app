059728d8ff1d00302f7345a2aa9b8159d93beb52
Joseph Thompson <joe.thompson838@gmail.com>
2024-07-12 15:44:01 -0400

    fix bug with next page

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -11,7 +11,7 @@ const CardList = async ({ page, cat }) => {
 
   const hasPrev = POST_PER_PAGE * (page - 1) > 0;
 
-  const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;
+  const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE <= count;
   return (
     <section className={styles.container}>
       <h1 className={styles.title}>Recent Posts</h1>
--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,4 @@ export const LIGHT_THEME = 'light';
 export const DARK_THEME = 'dark';
 export const ICON_SIZE = '24px';
 export const UNAUTHENTICATED = 'unauthenticated';
-export const POST_PER_PAGE = 2;
+export const POST_PER_PAGE = 3;
